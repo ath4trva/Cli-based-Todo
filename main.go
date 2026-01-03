3c46621e3467889eafeffcb87d35114867630a59
ath4trva <atharvapatil8021@gmail.com>
2026-01-03 22:33:24 +0530

    Completed Functionality

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-import "fmt"
+
 
 func main() {
 	todos:= Todos{}
 	todos.add("Learn Go")
 	todos.add("Build a Todo App")
-	
-	fmt.Printf("%+v\n\n", todos)
-	todos.delete(0)
-	fmt.Printf("%+v\n", todos)
+	todos.toggle(0)
+	todos.print()
 }
+	
+	
